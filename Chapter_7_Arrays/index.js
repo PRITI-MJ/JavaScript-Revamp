b5e2cc3d57b8e79d0adbf06d1ddd62532e6f2cb2
Priti <pritimukherjee650@gmail.com>
2025-10-07 00:01:48 +0530

    Adding files for chapter 6

---
+++ Chapter_7_Arrays/index.js
@@ -0,0 +1,14 @@
+//Array
+
+// let marks1 = 100;
+// let marks2 = 50;
+// let marks3 = 70;
+// let marks4 = 80;
+
+//instead of storing it indivually, we can store it in array
+let marks = [100, 50, 70, 80, 90]
+console.log(marks); //[ 100, 50, 70, 80, 90 ]
+console.log(marks.length); //5
+
+//In javascript, we can store heterogenous type of data unlike c++, java.
+let arr = [100, 30, "Rohit", true]
