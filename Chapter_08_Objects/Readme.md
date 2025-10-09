51dc3ce1e158439e347fe534888c6e161411e459
Priti <pritimukherjee650@gmail.com>
2025-10-09 23:53:16 +0530

    Adding remaining files for Chapter 8

--- Chapter_08_Objects/Readme.md
+++ Chapter_08_Objects/Readme.md
@@ -1,3 +1,35 @@
 -> We should not prefer for-in loop for iteration in objects
 -> Rather than we should use for of loop, But that is not possible in objects directly.
 -> As we know, Object.keys and Object.values returns array, so first we change it to array and then perform the for-of loop
+
+In deep copy, we can create independent copies of nested objects. 
+(It is done by using structuredClone(user) function)
+
+
+In shallow copy, we can only create independent copies at one level, and not work for nested object.
+(It is done by spread operator)
+
+
+What is limitations of structuredClone()?
+-> structuredClone() does not duplicate the prototype chain. If a class instance is cloned, the returned object will be a plain object, losing its original class identity and methods.
+-> Private elements (fields and methods) within classes are not duplicated by structuredClone()
+
+
+How arrays are stored in the form of objects in javascript?
+-> const arr = [10, 20, 30, 40];
+
+//Object representation of array stored in JS(behind the scene)
+{
+    0:10,
+    1:20,
+    2:30,
+    3:40
+}
+Here index is converted into keys(which treats the index as string) and values are converted into values
+thats why we access array as arr[0]
+That is why arrays are treated as objects
+
+KEYS in object can be String and symbol
+
+
+
--- Chapter_08_Objects/index.js
+++ Chapter_08_Objects/index.js
@@ -103,7 +103,186 @@ amount
 home address
 adhar */
 
+const user3 = user;
 //iterating in values
-for(let values in Object.values(user)){
+for(let values of Object.values(user3)){
     console.log(values);
 }
+/* Rohit
+90
+5000
+dwarka
+1234
+*/
+
+// if we want to keys and values together, we can use Object.entries
+console.log(Object.entries(user));
+/*[
+    [ 'name', 'Rohit' ],
+    [ 'age', 90 ],
+    [ 'amount', 5000 ],
+    [ 'home address', 'dwarka' ],
+    [ 'adhar', 1234 ]
+] */
+
+
+// we can print the keys and values separately
+for(let [keys, values] of Object.entries(user)){
+  console.log(keys, values);
+  /*
+name Rohit
+age 90
+amount 5000
+home address dwarka
+adhar 1234 
+*/
+}
+
+
+//we can also write function inside objects
+const user4 = {
+  name: "Rohit",
+  age: 20,
+  emailId: "negi@gmail.com",
+  amount: 3400,
+  greeting: function(){
+    console.log(`Strike is coming on 18 october ${user4.name}`);
+    return 20;
+  }
+}
+
+const val = user4.greeting(); //Strike is coming on 18 october Rohit
+console.log(val); //20
+
+
+const user5 = {
+  name: "Mohan",
+  account: 201,
+}
+
+user5.greeting = user4.greeting;
+
+user5.greeting(); //Strike is coming on 18 october Rohit
+// so as we hardcoded with user4.name, it is taking the name from the user4, and not updating it with user5.name
+//for fixing this issue, we use this keyword
+
+
+
+const user6 = {
+  name: "Rohit",
+  age: 20,
+  emailId: "negi@gmail.com",
+  amount: 3400,
+  greeting: function(){
+    //here this keyword refers to the object which is being called
+    console.log(`Strike is coming on 18 october ${this.name}`);
+    return 20;
+  }
+}
+
+const val1 = user6.greeting(); //Strike is coming on 18 october Rohit
+console.log(val1); //20
+
+
+const user7 = {
+  name: "Mohan",
+  account: 201,
+}
+
+user7.greeting = user6.greeting; 
+
+user7.greeting(); //Strike is coming on 18 october Mohan
+
+
+
+//Nested Object
+const user9 = {
+  name: "Rohit",
+  age: 20,
+  emailId: "negi@gmail.com",
+  amount: 3400,
+  address: {
+    city: "kotdwar",
+    state: "Uttarkhand"
+  }
+}
+
+console.log(user9.address.city); //kotdwar
+
+//as we know object is copied by using copy by reference
+//if we want to do copy by value, we use spread operator
+// But here only at one level it will do independent copies
+// It will not work for nested objects (Shallow copy)
+const user8 = {...user9};
+user8.name = "Priti";
+user8.address.city = "Dhanbad"
+console.log(user9);
+
+/*{
+  name: 'Rohit',
+  age: 20,
+  emailId: 'negi@gmail.com',
+  amount: 3400,
+  address: { city: 'dhanbad', state: 'Uttarkhand' }
+} */
+
+
+console.log(user8);
+/*{
+  name: 'Priti',
+  age: 20,
+  emailId: 'negi@gmail.com',
+  amount: 3400,
+  address: { city: 'Dhanbad', state: 'Uttarkhand' }
+} */
+
+
+
+//Deep copy
+const user10 = structuredClone(user9)
+user10.name = "Priti";
+user10.address.state = "Jharkhand";
+console.log(user9);
+/* {
+  name: 'Rohit',
+  age: 20,
+  emailId: 'negi@gmail.com',
+  amount: 3400,
+  address: { city: 'Dhanbad', state: 'Uttarkhand' }
+}*/
+console.log(user10);
+/*{
+  name: 'Priti',
+  age: 20,
+  emailId: 'negi@gmail.com',
+  amount: 3400,
+  address: { city: 'Dhanbad', state: 'Jharkhand' }
+} */
+
+
+//We can also assign values to keys as number, as behind the scene JS store the keys and values in the form of strings
+const user11 = {
+  name: "Rohit",
+  age: 20,
+  0: 100,
+  2: "Mohan"
+}
+
+//we can access it either of these, we can't able to use user.0
+console.log(user[0]);
+//console.log(user["0"]);
+
+
+//using symbol in objects
+const sym = Symbol("id");
+
+const user12 = {
+  name: "Rohit",
+  age: 20,
+  0: 100,
+  2: "Mohan",
+  [sym]: "Hello Ji"
+}
+
+
+console.log(user12[sym]); //Hello Ji
