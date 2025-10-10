db69d4b2715dc78178ba0b7b3e844bafc911bc14
Priti <pritimukherjee650@gmail.com>
2025-10-10 23:12:14 +0530

    Adding files for Chapter 9

---
+++ Chapter_09_Functions/index.js
@@ -0,0 +1,16 @@
+//Functions
+
+function greeting(){
+    console.log("Hello Coder Army, strike is on 18th October, 2025")
+}
+
+greeting(); //Hello Coder Army, strike is on 18th October, 2025
+
+function addNumber(num1, num2) {
+    const sum = num1+num2;
+    console.log(sum);
+}
+
+addNumber(3,4); //7
+addNumber(6,7); //13
+
