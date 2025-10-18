788715f39c0e360d4f686ccf2dfd7e0cb5d56b13
Priti <pritimukherjee650@gmail.com>
2025-10-18 18:49:07 +0530

    Adding remaining files for Chapter 15

--- Chapter_15_EventHandler/Readme.md
+++ Chapter_15_EventHandler/Readme.md
@@ -2,3 +2,19 @@ Event: Mouse move, click. double click
 Event Listner: Listening the event(click)
 Event Action: Strike is coming
 
+
+
+3 phases of EVENT LISTENER:-
+Capture Phase
+Target Phase
+Bubling Phase
+
+Capture Phase => If its true, then it will print while going down the DOM tree.
+If it is false, then it will print during the bubling phase.
+(By default it is set to false during event)
+
+
+Target Phase => Once it reach the target phase, bubling phase starts
+
+Bubling phase => It will start executing the event as soon as it does up in the DOM tree.
+
--- Chapter_15_EventHandler/first.js
+++ Chapter_15_EventHandler/first.js
@@ -17,14 +17,99 @@
 //     element.style.backgroundColor="brown";
 // }
 
-const element = document.getElementById("first");
-element.addEventListener('click', () => {
-    element.textContent = "Strike is coming"
-})
+// const element = document.getElementById("first");
+// element.addEventListener('click', () => {
+//     element.textContent = "Strike is coming"
+// })
 
-element.addEventListener('click', () => {
-    element.style.backgroundColor = "brown";
-})
+// element.addEventListener('dblclick', () => {
+//     element.textContent = "I am the best"
+// })
+
+// element.addEventListener('click', () => {
+//     element.style.backgroundColor = "brown";
+// })
+
+// element.addEventListener('mouseenter', () => {
+//     element.style.backgroundColor = "brown";
+// })
+
+// element.addEventListener('mouseleave', () => {
+//     element.style.backgroundColor = "brown";
+// })
 
 
+// const child1 = document.getElementById("child1");
+// child1.addEventListener("click",() => {
+//     child1.textContent = "I am clicked";
+// })
 
+//But if we want to apply this to all child, then we don't have to do manually
+// const parent = document.getElementById("parent");
+
+// for(let child of parent.children){
+//     console.log(child);
+//     child.addEventListener("click", () => {
+//         child.textContent = "I am Clicked";
+//     })
+// }
+
+//But its a long process, instead of that we can use e.target
+function handleClick(e){
+    e.target.textContent = "I am Clicked";
+}
+
+
+//To add eventlistner
+// parent.addEventListener("click", (e) => {
+//     e.target.textContent = "I am Clicked";
+// })
+parent.addEventListener("click",handleClick);
+
+//remove eventListner
+//parent.removeEventListener("click", handleClick);
+
+
+// const grandparent = document.getElementById("grandparent");
+// grandparent.addEventListener("click", () => {
+//     console.log("Grandparent is clicked");
+// })
+
+// const parent = document.getElementById("parent");
+// parent.addEventListener("click", () => {
+//     console.log("parent is clicked");
+// })
+
+// const child = document.getElementById("child");
+// child.addEventListener("click", () => {
+//     console.log("child is clicked");
+// })
+//Output
+//child is clicked
+//parent is clicked
+// Grandparent is clicked
+
+// const grandparent1 = document.getElementById("grandparent");
+// grandparent1.addEventListener("click", () => {
+//     console.log("Grandparent is clicked");
+// }, true)
+
+// const parent1 = document.getElementById("parent");
+// parent1.addEventListener("click", () => {
+//     console.log("parent is clicked");
+// }, true)
+
+// const child1 = document.getElementById("child");
+// child1.addEventListener("click", () => {
+//     console.log("child is clicked");
+// }, true)
+
+//output
+// Grandparent is clicked
+// parent is clicked
+// child is clicked
+
+
+//e => a event object which stores various information
+// So we can stop bubling by e.stopPropagation();
+//e.target => we can know, where it got triggered
