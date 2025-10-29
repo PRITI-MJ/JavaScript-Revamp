90a6d60f3a8de43f6c9226d57d68c1f5a38004b4
Priti <pritimukherjee650@gmail.com>
2025-10-29 23:40:20 +0530

    adding files for chapter 19

---
+++ Chapter_19_Promises/Readme.md
@@ -0,0 +1,22 @@
+Promise is a object which has 3 states:-
+1. Pending:- when we request something from the server, it is in Pending state.
+2. Fulfilled:- When we got the correct data, that means the promise got fulfilled.
+3. Reject:- when we don't get any data or an error, means the promise is rejected.
+
+Fullfilled means => If server replies back either there is no data in the api, still it will considered as fullfilled and not rejected.
+
+When reject will work?
+-> In case of:-
+    1. Internet Down
+    2. Server Down
+    3. DNS Down
+
+
+What is JSON?
+-> Javascript Object Notation
+
+Difference between JSON and JS object?
+-> JSON object will be understood by every language like python, c++, java, js, etc.
+-> JS object will only be understood by JS.
+-> In JSON, undefined is not allowed, function is also not present where as In JS object it is allowed.
+-> JSON is in string format where as in JS object it is a Javascript Object.
---
+++ Chapter_19_Promises/first.js
@@ -0,0 +1,87 @@
+console.log("Hello World Start");
+
+
+//code
+
+
+//const p1 = fetch("https://api.github.com/users");
+//console.log(p1); //Promise { <pending> }
+
+//fulfilled, rejected
+
+//const p2 = p1.then((response) => {
+    //console.log(response.json()); 
+    //.json() converts the response into json format(readible format))
+    //but it also returns a promise {<pending>} because it takes some time to convert into json format
+
+    //return response.json();
+//})
+
+// p2.then((data) =>{
+//     console.log(data);
+// })
+
+
+
+
+//BETTER WAY TO WRITE THE ABOVE CODE USING CHAINING OF .then()
+
+//PROMISE CHAINING
+fetch("https://api.github.com/users")
+.then((response) => {
+
+    if(!response.ok){
+        throw new Error("Data is not present in the server");
+    }
+    return response.json();
+}).then((data) => {
+    // console.log(data);
+
+    for(let i=0; i< data.length; i++){
+    const parent = document.getElementById("first");
+    const image = document.createElement("img");
+    image.src = data[i].avatar_url;
+    image.style.height = "100px";
+    image.style.width = "100px";
+    parent.append(image);
+
+    }
+
+})
+.catch((error) => {
+    const parent = document.getElementById("first");
+    parent.textContent = error.message;
+    console.log("Error occurred:", error);
+})
+
+
+console.log("Hello World End");
+
+
+// const j1 = {
+//     name: "Rohit",
+//     age: 30,
+//     address: "dwarka",
+//     c : undefined,
+// }
+//JSON will ignore the key with undefined value
+
+//convert to json
+// const jsonFormat = JSON.stringify(j1);
+// console.log(jsonFormat);
+//{"name":"Rohit","age":30,"address":"dwarka"}
+
+
+//creating json object
+//we can use backtick also for creating json object 
+// const jsonObj = 
+// `{"name":"Rohit",
+// "age": 30
+// "address":"dwarka"
+// }`;
+
+//convert back to object
+// const objFormat = JSON.parse(jsonObj);
+// console.log(objFormat);
+//{ name: 'Rohit', age: 30, address: 'dwarka' }
+
